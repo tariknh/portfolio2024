e3726acbe87aa4c15819b6192e4d3b112ea3beba
Tarik <hajibtarik@gmail.com>
2024-09-03 22:33:28 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Hero from "./components/Hero";
-
 export default function Home() {
   return (
     <main className="h-screen">
       <section className="h-screen bg-slate-700"></section>
-      <Hero />
+      {/* <Hero /> */}
     </main>
   );
 }
