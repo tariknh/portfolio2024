674637b17ce3cadcd0bb010be4dc04c66ceaad07
Tarik <112902780+tariknh@users.noreply.github.com>
2024-09-03 22:35:17 +0200

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,15 +13,18 @@ const buttonVariants = cva(
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        outline: "border-[1px] rounded-3xl text-white underline-offset-4 hover:underline",
+        shortOutline: "border-[1px] rounded-[10rem] text-white underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2",
+        long: "h-12 px-6 py-2",
+        short: "h-12 px-5 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
