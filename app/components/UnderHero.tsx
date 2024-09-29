ddfed328f22c8066f32e80cd5be9d0c3ae7de9e1
Tarik <hajibtarik@gmail.com>
2024-09-30 00:01:50 +0200

    Button animation

--- app/components/UnderHero.tsx
+++ app/components/UnderHero.tsx
@@ -1,9 +1,51 @@
-const UnderHero = ({ scrollYProgress }: any) => {
+import Hoverbutton from "@/components/ui/Hoverbutton";
+import { motion, useInView } from "framer-motion";
+import Image from "next/image";
+import { useRef } from "react";
+
+const Project = () => {
   return (
-    <div className="h-screen bg-primary">
-      <div className="">
-        <span>RECENT</span>
-        <span>WORK</span>
+    <div className="flex md:justify-between w-full h-full gap-5 text-white">
+      <div className="max-w-lg md:max-w-none flex flex-col justify-center">
+        <h2 className="text-xl md:text-6xl font-bold">Lofothuset</h2>
+        <h2 className="text-xs font-thin text-end">
+          Branding, Marketing, Design
+        </h2>
+        {/* <Button className="w-40" size={"short"} variant={"outline"}>
+          View case
+        </Button> */}
+        <Hoverbutton />
+      </div>
+      <div className="rounded-lg basis-2/4 overflow-hidden relative w-full">
+        <Image
+          className="aspect-video h-full"
+          src="/projects/lofothuset.png"
+          alt="lofothuset"
+          fill
+        />
+      </div>
+    </div>
+  );
+};
+
+const UnderHero = ({ scrollYProgress }: any) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  return (
+    <div className="py-[20vh] bg-primary grid grid-rows-6">
+      <motion.span
+        animate={{
+          y: isInView ? "0%" : "50%",
+          opacity: isInView ? 1 : 0,
+          transition: { duration: 0.5, ease: "easeInOut" },
+        }}
+        ref={ref}
+        className="justify-self-center row-start-2 text-[10vw] text-center w-full text-white font-bold"
+      >
+        RECENT <span className="font-outline-2 text-primary">WORK</span>
+      </motion.span>
+      <div className="row-start-4 row-span-2 px-40">
+        <Project />
       </div>
     </div>
   );
---
+++ components/ui/Hoverbutton.tsx
@@ -0,0 +1,105 @@
+import { useRef } from "react";
+
+import { useGSAP } from "@gsap/react"; // <-- import the hook from our React package
+import gsap from "gsap"; // <-- import GSAP
+import { Button } from "./button";
+
+gsap.registerPlugin(useGSAP);
+
+export default function App() {
+  const button: any = useRef(null);
+  const wrapRef = useRef<HTMLDivElement>(null);
+  const clipRef = useRef<HTMLDivElement>(null);
+
+  const durationSetting = 0.4;
+  const easeSetting = "power2.out";
+
+  const getPercentTop = (el: HTMLDivElement, e: MouseEvent) => {
+    const elTop = el.getBoundingClientRect().top;
+    const mouseTop = e.clientY - elTop;
+    return (mouseTop / el.offsetHeight) * 100;
+  };
+
+  const getPercentLeft = (el: HTMLDivElement, e: MouseEvent) => {
+    const elLeft = el.getBoundingClientRect().left;
+    const mouseLeft = e.clientX - elLeft;
+    return (mouseLeft / el.offsetWidth) * 100;
+  };
+
+  useGSAP((context, contextSafe: any) => {
+    const wrapElement = wrapRef.current;
+    const clipElement = clipRef.current;
+    if (!wrapElement || !clipElement) return () => {};
+
+    const handleMouseEnter = (e: MouseEvent) => {
+      const percentTop = getPercentTop(wrapElement, e);
+      const percentLeft = getPercentLeft(wrapElement, e);
+
+      gsap.set(clipElement, { display: "flex" });
+      gsap.fromTo(
+        clipElement,
+        { clipPath: `circle(0% at ${percentLeft}% ${percentTop}%)` },
+        {
+          clipPath: `circle(141.4% at ${percentLeft}% ${percentTop}%)`,
+          duration: durationSetting,
+          ease: easeSetting,
+        }
+      );
+    };
+
+    const handleMouseLeave = (e: MouseEvent) => {
+      const percentTop = getPercentTop(wrapElement, e);
+      const percentLeft = getPercentLeft(wrapElement, e);
+
+      gsap.to(clipElement, {
+        clipPath: `circle(0% at ${percentLeft}% ${percentTop}%)`,
+        overwrite: true,
+        duration: durationSetting,
+        ease: easeSetting,
+      });
+    };
+
+    wrapElement.addEventListener("mouseenter", handleMouseEnter);
+    wrapElement.addEventListener("mouseleave", handleMouseLeave);
+
+    // 👍 we remove the event listener in the cleanup function below.
+    return () => {
+      // <-- cleanup
+      wrapElement.removeEventListener("mouseenter", handleMouseEnter);
+      wrapElement.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, {}); // <-- scope for selector text (optional)
+
+  return (
+    <Button
+      data-btn="wrap"
+      className="w-40"
+      size={"short"}
+      variant={"outline"}
+      ref={wrapRef}
+      style={{ position: "relative", overflow: "hidden" }}
+    >
+      <div
+        data-btn="clip"
+        ref={clipRef}
+        aria-hidden="true"
+        className="text-background flex items-center justify-center text-sm"
+        style={{
+          display: "none",
+          position: "absolute",
+          textAlign: "center",
+          inset: 0,
+          color: "black",
+          backgroundColor: "white", // Set the circle color (e.g., semi-transparent blue)
+          //borderRadius: "50%", // Make it a circle
+          pointerEvents: "none", // Ensures the circle doesn't interfere with mouse events
+        }}
+      >
+        {/* Content inside the clip */}
+        <span className="text-background!important">View case</span>
+      </div>
+      {/* Wrap button content */}
+      View case
+    </Button>
+  );
+}
